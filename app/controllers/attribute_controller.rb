56e0cf876b7ea303ee6d53104acf9cf9162ef0e8
David Ackerson <david.ackerson2@gmail.com>
2015-01-18 16:48:23 -0400

    Added basic upload functionality

--- app/controllers/attribute_controller.rb
+++ app/controllers/attribute_controller.rb
@@ -1,7 +1,15 @@
+require 'rmagick'
+
 class AttributeController < ApplicationController
 
     def upload
-        uploaded_io = params[:image]
+        img = Magick::Image::from_blob(params[:image].read)
+        puts img.size
+        puts img[0].format
+        puts img[0].filename
+
+        # @response.headers["Content-type"] = img.mime_type
+        send_data img[0].to_blob, type: "image/#{img[0].format.downcase}"
     end
 
 end

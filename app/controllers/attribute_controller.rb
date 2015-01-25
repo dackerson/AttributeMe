f64b7e68aef88efe63fce848242c6b964dddfa85
David Ackerson <david.ackerson2@gmail.com>
2015-01-24 21:41:59 -0400

    Adds arbitrary banner at bottom of image

--- app/controllers/attribute_controller.rb
+++ app/controllers/attribute_controller.rb
@@ -3,13 +3,31 @@ require 'rmagick'
 class AttributeController < ApplicationController
 
     def upload
-        img = Magick::Image::from_blob(params[:image].read)
-        puts img.size
-        puts img[0].format
-        puts img[0].filename
+        file = params[:image]
+        img = Magick::Image::from_blob(file.read)[0]
+
+        width = img.columns
+        height = img.rows
+
+        pixels = img.dispatch 0, 0, width, 500, "RGB"
+
+        banner_img = Magick::Image.constitute width, 500, "RGB", pixels
+
+        new_img = Magick::Image.new(width, height + 100) {
+            self.format = img.format
+        }
+
+        new_img.composite!(img, 0, 0, Magick::OverCompositeOp)
+        new_img.composite!(banner_img, 0, height, Magick::OverCompositeOp)
+
+        out = new_img
+
+        # puts out.methods
+        # puts out.rows
+        # puts out.columns
 
         # @response.headers["Content-type"] = img.mime_type
-        send_data img[0].to_blob, type: "image/#{img[0].format.downcase}"
+        send_data out.to_blob, type: file.content_type, filename: file.original_filename
     end
 
 end
